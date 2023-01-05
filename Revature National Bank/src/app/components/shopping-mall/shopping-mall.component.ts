9a899f71b5949b1706f97f412b437463ea8c2bbb
Justin Pan <105833041+jrprocks@users.noreply.github.com>
2023-01-05 07:26:08 -0500

    .

--- Revature National Bank/src/app/components/shopping-mall/shopping-mall.component.ts
+++ Revature National Bank/src/app/components/shopping-mall/shopping-mall.component.ts
@@ -32,13 +32,15 @@ export class ShoppingMallComponent implements OnInit {
   buy(shop: Account) {
     //change "to" account to the logged in user's account -> add to account transaction page
     let trans:Transaction;
-    if ((shop.type)==="Job"){
+    if ((shop.type)==="Work a job!"){
       trans = {id:0, linkedTo:{"id":1}, from:{"id":shop.id}, to:this.acc, amount:250, date_of_trans:"2022-11-06", description:shop.type}
     } else {
       let transAmount = (Math.floor(Math.random() * 25) + 7.95)*-1;
       trans = {id:0, linkedTo:{"id":1}, from:{"id":shop.id}, to:this.acc, amount:transAmount, date_of_trans:"2022-11-06", description:shop.type}
     }
     this.transactionService.postAuthTransaction(trans).subscribe(transs=>this.transs=transs)
+
+    document.getElementById('transactioncomplete').innerHTML = 'Transaction complete!';
 
   }
 }
